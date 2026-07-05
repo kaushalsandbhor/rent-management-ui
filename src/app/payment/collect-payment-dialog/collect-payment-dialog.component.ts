ff7710a9abdfd2709969aeba9fff55fea585387c
kaushalsandhor <kaushalsandhor17@gmail.com>
2026-07-05 15:53:51 +0530

    console output

--- src/app/payment/collect-payment-dialog/collect-payment-dialog.component.ts
+++ src/app/payment/collect-payment-dialog/collect-payment-dialog.component.ts
@@ -2,8 +2,18 @@ import { Component, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   MAT_DIALOG_DATA,
-  MatDialogModule
+  MatDialogModule,
+  MatDialogRef
 } from '@angular/material/dialog';
+
+import { FormsModule } from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
 
 import { Dashboard } from '../../models/dashboard';
 
@@ -12,17 +22,54 @@ import { Dashboard } from '../../models/dashboard';
   standalone: true,
   imports: [
     CommonModule,
-    MatDialogModule
+    FormsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule
   ],
   templateUrl: './collect-payment-dialog.component.html',
   styleUrl: './collect-payment-dialog.component.css'
 })
 export class CollectPaymentDialogComponent {
 
+  paidAmount!: number;
+
+  paymentDate = new Date();
+
+  paymentMode = 'UPI';
+
+  remarks = '';
+
+  paymentModes = [
+    'UPI',
+    'Cash',
+    'Cheque'
+  ];
+
   constructor(
     @Inject(MAT_DIALOG_DATA)
-    public data: Dashboard
-  ) {
+    public data: Dashboard,
+    public dialogRef: MatDialogRef<CollectPaymentDialogComponent>
+  ) {}
+
+  save() {
+
+    console.log({
+      tenantId: this.data.tenantId,
+      billingMonth: this.data.billingMonth,
+      billingYear: this.data.billingYear,
+      paidAmount: this.paidAmount,
+      paymentDate: this.paymentDate,
+      paymentMode: this.paymentMode,
+      remarks: this.remarks
+    });
+
+    this.dialogRef.close(true);
+
   }
 
 }
