1bae3deff78020078a5861d80e2f51fcf7365915
kaushalsandhor <kaushalsandhor17@gmail.com>
2026-07-19 00:04:56 +0530

    URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl1: 'https://rent-management-api-production.up.railway.app/api',
-  apiUrl: 'http://localhost:8080/api'
+  apiUrl: 'https://rent-management-api-production.up.railway.app/api',
+  apiUrl1: 'http://localhost:8080/api'
 };
